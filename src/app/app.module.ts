1bdf834d304944bfa3c61fc0067b0ae689af486a
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2017-05-26 20:06:26 -0300

    componente cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
